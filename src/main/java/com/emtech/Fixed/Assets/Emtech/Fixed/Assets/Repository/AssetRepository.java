e9014b38c09afa3c825e86404a160d15cd750eba
Willy.nyawira <nyawiramwangi394@gmail.com>
2024-05-22 15:01:50 +0300

    Deleted master

--- src/main/java/com/emtech/Fixed/Assets/Emtech/Fixed/Assets/Repository/AssetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.emtech.Fixed.Assets.Emtech.Fixed.Assets.Repository;
-
-import com.emtech.Fixed.Assets.Emtech.Fixed.Assets.Entity.Asset;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssetRepository extends JpaRepository<Asset, Long> {
-}
